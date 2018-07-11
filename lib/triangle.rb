061b230ae4f9c80a4e1dfc9a746a4aceb32b49d8
Terri Riggle <codeofarmour@outlook.com>
2018-07-11 04:01:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,11 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :s1, :s2, :s3
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
   
   def kind
   end
